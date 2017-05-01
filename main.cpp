5a444c3df704743f90181bdc14212738a18a50a8
jiyi <770864410@qq.com>
2017-05-01 17:06:52 +0900

    add test case; small fixes

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include <vector>
 #include <ctime>
 #include "TransactionDB.hpp"
+#include "Miner.hpp"
 #include "MINIT.hpp"
 #include "MIWI.hpp"
+#include "MAFIA.hpp"
 
 void test_MINIT()
 {
-    TransactionDB D("/Users/jiyi/Documents/ut/lab/proj/data/mushroom.dat", DB_type::BITSET, SORT_type::ASC);
-    Miner* miner = new MINIT(&D, 1000.0/8124.0, D.dim());
+    TransactionDB D("/Users/jiyi/Documents/ut/lab/proj/data/mushroom.dat", DB_type::BITSET);
+    Miner* miner = new MINIT(&D, 100.0/8124.0, D.dim());
     miner->mine();
     if (miner->check())
     {
@@ -20,8 +22,8 @@ void test_MINIT()
 
 void test_MIWI()
 {
-    TransactionDB D("/Users/jiyi/Documents/ut/lab/proj/data/mushroom.dat", DB_type::VECTOR, SORT_type::DSC);
-    Miner* miner = new MIWI(&D, 1000.0/8124.0);
+    TransactionDB D("/Users/jiyi/Documents/ut/lab/proj/data/mushroom.dat", DB_type::VECTOR);
+    Miner* miner = new MIWI(&D, 100.0/8124.0);
     
     miner->mine();
     if (miner->check())
@@ -33,8 +35,23 @@ void test_MIWI()
     delete miner;
 }
 
+void test_MAFIA()
+{
+    TransactionDB D("/Users/jiyi/Documents/ut/lab/proj/data/mushroom.dat", DB_type::VECTOR);
+    Miner* miner = new MAFIA(&D, 100.0/8124.0);
+    
+    miner->mine();
+    if (miner->check())
+    {
+        printf("result is valid.\n");
+        miner->print_result();
+    }
+    
+    delete miner;
+}
+
 int main(int argc, const char * argv[])
 {
-    test_MIWI();
+    test_MAFIA();
     return 0;
 }
