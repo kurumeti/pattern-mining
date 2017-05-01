821ed86cc98efa6ac5e4aa1ecf3aaffd55b8da83
jiyi <770864410@qq.com>
2017-05-01 17:15:33 +0900

    subset testing decoupled

--- MIWI.hpp
+++ MIWI.hpp
@@ -1,4 +1,5 @@
 #include <stack>
+
 using namespace std;
 
 #define PRUNING 1
@@ -32,21 +33,6 @@ protected:
             support = _support;
         }
     };
-    struct M_Node
-    {
-        vector<M_Node*> children;
-        M_Node* father = NULL;
-        vector<int> path;
-        M_Node(){}
-        M_Node(vector<int>::iterator l, vector<int>::iterator r)
-        {
-            while (l != r)
-            {
-                path.push_back(*l);
-                l++;
-            }
-        }
-    };
     map<int, Item> items;
     vector<Item> sorted_items;
     Node* tree;
@@ -59,7 +45,7 @@ public:
     ~MIWI()
     {
         delete_tree(tree);
-        delete_tree(min_tree);
+        M_Node::delete_tree(min_tree);
     }
     Node* build_tree()
     {
@@ -78,91 +64,6 @@ public:
         {
             printf("%d %d\n", i->first, i->second.support);
         }
-    }
-    bool insert_min_tree(M_Node* temp, vector<int>& tids, vector<int>::iterator v)
-    {
-        bool children_covered = false;
-        vector<int>::iterator i = temp->path.begin();
-        while (i != temp->path.end() && v != tids.end() && *i == *v)
-        {
-            v++;i++;
-        }
-        if (i == temp->path.begin() && temp != min_tree)
-        {
-            return false;
-        }
-        if (i == temp->path.end())
-        {
-            for (vector<M_Node*>::iterator j = temp->children.begin(); j != temp->children.end(); j++)
-            {
-                if (insert_min_tree(*j, tids, v))
-                {
-                    children_covered = true;
-                    break;
-                }
-            }
-        }
-        if (i != temp->path.end() || !children_covered)
-        {
-            if (i != temp->path.end())
-            {
-                M_Node* t2 = new M_Node(i, temp->path.end());
-                for (vector<M_Node*>::iterator j = temp->children.begin(); j != temp->children.end(); j++)
-                {
-                    t2->children.push_back(*j);
-                }
-                temp->children.clear();
-                temp->children.push_back(t2);
-                i--;
-                while (temp->path.back() != *i)
-                {
-                    temp->path.pop_back();
-                }
-            }
-            M_Node* t1 = new M_Node(v, tids.end());
-            temp->children.push_back(t1);
-        }
-        return true;
-    }
-    bool check_minimal(const vector<int>& tids)
-    {
-        vector<int> ranks;
-        for (vector<const int>::iterator i = tids.begin(); i != tids.end(); i++)
-        {
-            ranks.push_back(DB->rank[*i]); //put rank in here
-        }
-        reverse(ranks.begin(), ranks.end()); // dual is ok. if not reverse here then just reverse the corresponding inequation in check_min_tree
-        if (!check_min_tree(min_tree, ranks, ranks.begin()))
-        {
-            insert_min_tree(min_tree, ranks, ranks.begin());
-            return true;
-        }
-        return false;
-    }
-    bool check_min_tree(M_Node* temp, vector<int>& tids, vector<int>::iterator v)
-    {
-        vector<int>::iterator i = temp->path.begin();
-        while (i != temp->path.end() && v != tids.end())
-        {
-            if (*i < *v)
-            {
-                return false;
-            }
-            if (*i == *v)
-            {
-                i++;
-            }
-            v++;
-        }
-        if (i == temp->path.end())
-        {
-            if (temp->children.empty() && temp != min_tree) {return true;}
-            for (vector<M_Node*>::iterator j = temp->children.begin(); j != temp->children.end(); j++)
-            {
-                if (check_min_tree(*j, tids, v)) {return true;}
-            }
-        }
-        return false;
     }
     void insert_tree(Node* temp, map<int, Item>* form, const vector<int>& tids, int count)
     {
@@ -208,15 +109,6 @@ public:
         for (map<int, Node*>::iterator i = temp->children.begin(); i != temp->children.end(); i++)
         {
             delete_tree(i->second);
-        }
-        delete temp;
-    }
-    void delete_tree(M_Node* temp)
-    {
-        if (temp == NULL) {return;}
-        for (vector<M_Node*>::iterator i = temp->children.begin(); i != temp->children.end(); i++)
-        {
-            delete_tree(*i);
         }
         delete temp;
     }
@@ -311,7 +203,7 @@ public:
             pattern->support = i->support;
             if (pattern->support < threshold)
             {
-                if (check_minimal(pattern->get_tids()))
+                if (min_tree->check_minimal(pattern->get_tids(), DB))
                 {
                     result.push_back(new Itemset_VECTOR(pattern));
                     //DB->print_itemset(pattern);
@@ -388,7 +280,7 @@ public:
         long average_len = 0, minimal_num = 0, set_num = result.size();
         for (vector<Itemset*>::iterator i = result.begin(); i != result.end(); i++)
         {
-            if (check_minimal((*i)->get_tids()))
+            if (min_tree->check_minimal((*i)->get_tids(), DB))
             {
                 minimal_num += 1;
             }
@@ -411,5 +303,9 @@ public:
         dig_tree_stack(0, sorted_items);
         //dig_tree(tree, items);
         //antichain();
+        for (vector<Itemset*>::iterator i = result.begin(); i != result.end(); i++)
+        {
+            (*i)->reverse_self();
+        }
     }
 };
